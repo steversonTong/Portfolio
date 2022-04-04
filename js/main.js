43b8b8ebf6fb83cad0838fd7dd0848f198fdd4a6
Steve Tong <stevetong53@yahoo.com>
2022-04-04 00:58:43 -0700

    reinstall the correct swiper bundler

--- js/main.js
+++ js/main.js
@@ -39,3 +39,16 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
   el.addEventListener("click", toggleSkills);
 });
+
+let swiper = new Swiper(".portfolio_container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
