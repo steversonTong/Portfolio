99b72c0b37433f9b2287792e5736a1780f62484d
Steve Tong <stevetong53@yahoo.com>
2022-04-04 03:44:39 -0700

    added navbar shadow on scroll

--- js/main.js
+++ js/main.js
@@ -75,3 +75,10 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
+
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
